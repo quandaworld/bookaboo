1bd1ac4829cf4300d5f1384d0814e5fb8e5ed4f1
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-03-11 22:54:49 -0700

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://quandaworld:9BDgo85OYXzGIh5u@booklibrary.fokwhok.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookLibrary';
---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Hello Quan');
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+
+    // only run express server if db connection is successful
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
