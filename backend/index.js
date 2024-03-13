53a44f279bb3099390535bb8132cd2998b97a27e
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-03-12 23:07:35 -0700

    Install middleware for CORS policy

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ const app = express();
 // Middleware for parsing request body
 app.use(express.json());
 
+// Middleware for CORS policy
+app.use(cors());
+
 app.get('/', (request, response) => {
   console.log(request);
   return response.status(234).send('Hello Quan');
