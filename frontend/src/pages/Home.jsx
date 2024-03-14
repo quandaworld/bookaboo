56605c4e89eccd2fa8a7f7bfce4e50b07d2f3cd6
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-03-14 15:06:47 -0700

    Update home page layout - Fetch and display data on page - Enable two display options: table and cards

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+import { MdOutlineAddBox } from 'react-icons/md';
+import BooksCards from '../components/home/BooksCards';
+import BooksTable from '../components/home/BooksTable';
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState('table'); // Set default display to table
+
+  // Fetch book data, update component with fetched data, and track loading state
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('http://localhost:5555/books')
+      .then((response) => {
+        console.log(response.data);
+        setBooks(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div>Home</div>
+    <div className='p-4'>
+      <div className='flex justify-center items-center gap-x-4'>
+        <button
+          className='bg-sky-300 hover:bg-sky-600 hover:text-white px-4 py-1 rounded-lg'
+          onClick={() => setShowType('table')}
+        >
+          Table
+        </button>
+        <button
+          className='bg-sky-300 hover:bg-sky-600 hover:text-white px-4 py-1 rounded-lg'
+          onClick={() => setShowType('cards')}
+        >
+          Cards
+        </button>
+      </div>
+
+      <div className='flex justify-between items-center'>
+        <h1 className='text-3xl my-8'>My Library Pro</h1>
+        <Link to='/books/create' target='_blank'>
+          <MdOutlineAddBox className='text-sky-800 text-4xl' />
+        </Link>
+      </div>
+
+      {loading ? <Spinner /> : showType === 'table' ? <BooksTable books={books} /> : <BooksCards books={books} />}
+    </div>
   )
 }
 
