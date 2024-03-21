59218460e07ee4b21a58ab87ff6a5a8b725d0804
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-03-21 13:11:54 -0700

    Remove backend config.js

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDbUrl = 'mongodb+srv://quandaworld:9BDgo85OYXzGIh5u@booklibrary.fokwhok.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookLibrary';
