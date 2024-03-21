28c7957360ffd9ea39bea58610383ba87d033a93
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-03-21 16:53:24 -0700

    Update config file

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,9 @@
 export const PORT = 5555;
 
 // export const mongoDbUrl = 'mongodb+srv://quandaworld:9BDgo85OYXzGIh5u@booklibrary.fokwhok.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookLibrary';
-export const mongoDbUrl = 'mongodb+srv://quandaworld:9BDgo85OYXzGIh5u@booklibrary.fokwhok.mongodb.net/books-collection' || 'mongodb://localhost:27017/';
+export const mongoDbUrl = (() => {
+  const providedUrl = 'mongodb+srv://quandaworld:9BDgo85OYXzGIh5u@booklibrary.fokwhok.mongodb.net/books-collection';
+  const defaultUrl = 'mongodb://localhost:27017/';
+  
+  return providedUrl || defaultUrl;
+})();
